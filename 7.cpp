6e65651ec2228eb5e7450418590149ab0030d585
Daman Ahuja <142963733+DamanAhuja@users.noreply.github.com>
2025-05-01 02:24:41 +0530

    BFS

---
+++ 7.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void BFS(int start, vector<vector<int>>& adj, int V) {
+    vector<bool> visited(V, false);
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+
+    while (!q.empty()) {
+        int node = q.front(); q.pop();
+        cout << node << " ";
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main() {
+    int V = 5;
+    vector<vector<int>> adj(V);
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0, 4};
+    adj[3] = {1};
+    adj[4] = {2};
+
+    cout << "BFS starting from vertex 0: ";
+    BFS(0, adj, V);
+    return 0;
+}
