49a20ec5d3277e36386e84c587b972dab54e7c9c
Daman Ahuja <142963733+DamanAhuja@users.noreply.github.com>
2025-05-01 01:22:47 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int insertionSort(vector<int>& arr) {
+    int comparisons = 0;
+    for (int i = 1; i < arr.size(); ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && (++comparisons && arr[j] > key)) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    return comparisons;
+}
+
+int main() {
+    vector<int> arr = {5, 2, 4, 6, 1, 3};
+    int comps = insertionSort(arr);
+    for (int num : arr) cout << num << " ";
+    cout << "\nComparisons: " << comps << endl;
+    return 0;
+}
